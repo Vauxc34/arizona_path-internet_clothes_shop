f70d80c3c3c7384f2f12c31ac524fe62cbdeb536
Vauxc34 <kaxowy12@gmail.com>
2021-03-11 10:38:51 +0100

    arizona

---
+++ javascript/cursor.js
@@ -0,0 +1,14 @@
+/* const cursor = document.querySelector("div.cursor");
+let X = 600;
+let Y = 80;
+cursor.style.left = X + "px";
+cursor.style.top = Y + "px";
+
+
+/* cursor.addEventListener("mousemove", (move) => {
+ let X = 0;
+ let Y = 0;
+ cursor.style.left = `${move.clientX- 30 + "px"}`;
+ cursor.style.top = `${move.clientY- 30 + "px"}`;
+}) */
+
